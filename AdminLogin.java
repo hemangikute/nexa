10c44edcfdcce4febbcb00b1734bc2dcf761f3d9
hemangikute <59894124+hemangikute@users.noreply.github.com>
2020-03-21 15:05:09 -0500

    Add files via upload

---
+++ AdminLogin.java
@@ -0,0 +1,23 @@
+package org.iit.mmp.adminmodule;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AdminLogin {
+	
+	WebDriver driver;
+	
+	public AdminLogin(WebDriver driver) 
+	{
+		this.driver = driver;
+}
+
+	public void logintoAdmin(String username, String password)
+	{
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.name("admin")).click();
+
+	}
+
+}
---
+++ AdminPage.java
@@ -0,0 +1,49 @@
+package org.iit.mmp.adminmodule;
+
+import java.util.HashMap;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AdminPage {
+
+	WebDriver driver;
+	
+	public AdminPage(WebDriver driver) 
+	{
+		this.driver = driver;
+		
+	}
+
+	public String approvePatient(HashMap<String, String> hMap) throws Exception
+	{
+		
+		Select select = new Select(driver.findElement(By.id("search")));
+		select.selectByVisibleText("Pending");
+		
+		
+		driver.findElement(By.xpath("//a[contains(text(),'" +hMap.get("firstname")+"')]")).click();
+		
+		System.out.println("first name is "+hMap.get("firstname"));
+		Thread.sleep(5000);
+		
+		Select approvepatient = new Select(driver.findElement(By.id("sapproval")));
+		approvepatient.selectByVisibleText("Accepted");
+		
+		
+		driver.findElement(By.xpath("//input[@value='Submit']")).click();
+		
+		Alert alrt = driver.switchTo().alert();
+		String actual = alrt.getText();
+		alrt.accept();
+		return actual;
+
+		
+		
+		
+	}
+	
+	
+}
